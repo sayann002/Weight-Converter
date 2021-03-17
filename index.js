18d87a5ab407659a100e6e37b1693cf2bab7108e
Sayan Naskar <47991790+sayann002@users.noreply.github.com>
2021-03-17 22:17:39 +0530

    Changing Files

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const lbsInput = document.getElementById("lbsInput");
 const gramsOutput = document.getElementById("gramsOutput");
 const kgOutput = document.getElementById("kgOutput");
 const ozOutput = document.getElementById("ozOutput");
+const output = document.getElementById("output");
 
+output.style.visibility = "hidden";
 lbsInput.addEventListener("input", (e) => {
   let lbs = e.target.value;
+  output.style.visibility = "visible";
   //console.log(pounds);
   gramsOutput.innerHTML = lbs / 0.0022046;
   kgOutput.innerHTML = lbs / 2.20462;
